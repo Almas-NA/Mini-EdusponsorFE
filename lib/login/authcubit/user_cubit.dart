6d5c95bba64f22af959228b6b318806484789b40
Almas-NA <24mp2303@rit.ac.in>
2025-09-04 15:25:14 +0530

    user_cubit

---
+++ lib/login/authcubit/user_cubit.dart
@@ -0,0 +1,45 @@
+import 'package:bloc/bloc.dart';
+import 'package:edusponsor/Common/enums/response_type_enum.dart';
+import 'package:edusponsor/Common/network/dio_fetch_api.dart';
+import 'package:hive/hive.dart';
+import 'package:meta/meta.dart';
+import 'dart:developer' as developer;
+
+part 'user_state.dart';
+
+class UserCubit extends Cubit<UserState> {
+  UserCubit() : super(UserInitial());
+
+  Box box = Hive.box('eduSponsor');
+
+  Future<void> userLogin(String userName, String password) async {
+    emit(LoginLoading());
+    await Future.delayed(const Duration(seconds: 2));
+    try {
+      Map<String, String> requestHeaders = {
+        'password': password,
+        'username': userName,
+      };
+
+      final response = await FetchApi.postData(
+        endPoint: 'auth/login',
+        body: requestHeaders,
+        requiresToken: false,
+      );
+      if (response?['type'] == ServerResponseType.SUCCESS.name) {
+        List dataList =
+            ((response['responseData'] ?? {})['data'] ?? []) as List;
+        String accessToken = dataList[0]['accessToken'];
+        Map userDetails = dataList[0]['data'] ?? [];
+        box.put('accesstoken', accessToken);
+        emit(LoginSuccess(userDetails: userDetails));
+      } else if (response?['type'] == ServerResponseType.ERROR.name) {
+        List message = response['responseData']['message'];
+        emit(LoginFailed(responseData: message));
+      }
+    } on Exception catch (e) {
+      emit(LoginFailed(responseData: []));
+      developer.log('', error: 'Unhandled exception: ${e.toString()}');
+    }
+  }
+}
---
+++ lib/login/authcubit/user_state.dart
@@ -0,0 +1,24 @@
+part of 'user_cubit.dart';
+
+@immutable
+sealed class UserState {}
+
+final class UserInitial extends UserState {}
+
+final class LoginLoading extends UserState {}
+
+final class LoginSuccess extends UserState {
+
+  LoginSuccess({required this.userDetails});
+
+  final Map userDetails;
+  List<Object> get props => [userDetails];
+}
+
+final class LoginFailed extends UserState {
+
+  LoginFailed({required this.responseData});
+
+  final List responseData;
+  List<Object> get props => [responseData];
+}
