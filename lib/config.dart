c17668893c34336a99fd09766268501c4a178fbb
Almas-NA <24mp2303@rit.ac.in>
2025-09-04 15:17:00 +0530

    change1

---
+++ lib/config.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'common/color_generator.dart';
+
+//  ###########  Colors ########## //
+
+Color? primaryColor = Colors.blue[400];
+Color? secondaryColor = Colors.cyan;
+
+MaterialColor primaryShade = generateMaterialColor(primaryColor!);
+MaterialColor primaryShadeLight = generateMaterialColor(primaryShade.shade50);
+MaterialColor primaryShadeDark = generateMaterialColor(primaryShade.shade900);
+
+MaterialColor secondaryShade = generateMaterialColor(secondaryColor!);
+MaterialColor secondaryShadeLight =
+    generateMaterialColor(secondaryShade.shade50);
+MaterialColor secondaryShadeDark =
+    generateMaterialColor(secondaryShade.shade900);
+
+// ########
+final double shortSide =
+    MediaQueryData.fromView(WidgetsBinding.instance.window).size.shortestSide;
+final double longestSide =
+    MediaQueryData.fromView(WidgetsBinding.instance.window).size.longestSide;
+
+final double scalefactor = shortSide < 600
+    ? 1
+    : shortSide < 800
+        ? 1.4
+        : 1.6;
